c93148918a540246f5cca6e6e37b254668b5629b
Alex12062012 <154365189+Alex12062012@users.noreply.github.com>
2026-01-16 07:04:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -516,4 +516,4 @@ init_db()
 
 if __name__ == '__main__':
     # Lance l'application
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)ort=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
